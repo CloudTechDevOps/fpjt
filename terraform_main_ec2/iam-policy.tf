edf010d2c2a7aa7643b41f536e29975aea86f725
veera <veerababu.narni232@gmail.com>
2024-05-15 20:51:49 +0530

    updates project

--- terraform_main_ec2/iam-policy.tf
+++ terraform_main_ec2/iam-policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy_attachment" "iam-policy" {
   role = aws_iam_role.iam-role.name
-  # Just for testing purpose, don't try to give administrator access
+  # Just for testing purpose, don't try to give administrator access in production
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
